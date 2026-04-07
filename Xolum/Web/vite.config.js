e7929791ac3c90757e68e144193682bc29155928
DDamianZR <157549276+DDamianZR@users.noreply.github.com>
2026-04-07 11:25:57 -0600

    se corrije la web

--- Xolum/Web/vite.config.js
+++ Xolum/Web/vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
+  base: '/Xolum/',
   plugins: [
     react(),
     tailwindcss(),
